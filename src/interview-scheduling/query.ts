17eebe5610e983ebfdd400b3823e5c49d50642ae
Tam Nguyen <tamsanh@gmail.com>
2024-09-26 12:47:53 -0500

    Add extra notes

--- src/interview-scheduling/query.ts
+++ src/interview-scheduling/query.ts
@@ -1,4 +1,4 @@
-import { appointmentMocks, sample } from "@/interview-scheduling/data";
+import { appointmentMocks, sample, large } from "@/interview-scheduling/data";
 import { Appointment } from "./types";
 
 // export type TimeRange = {
@@ -36,22 +36,28 @@ import { Appointment } from "./types";
 // };
 
 export const getSchedules = (): Appointment[] => {
-  // TODO: Implement this function
 
-  // The first task to complete is the scheduling of employees and customers.
-  // The data structure describing availability is a list of `TimeRange` structs,
-  // which describes the start and end of a range of time that an individual is available.
+  //// --------------
+  //// TODO: Implement this function
+  //// The first task to complete is the scheduling of employees and customers.
+  //// The data structure describing availability is a list of `TimeRange` structs,
+  //// which describes the start and end of a range of time that an individual is available.
 
-  // If there is at least a 1 hour overlap between any time ranges for an employee and a customer,
-  // then an appointment can be scheduled for that overlap period.
+  //// If there is at least a 1 hour overlap between any time ranges for an employee and a customer,
+  //// then an appointment can be scheduled for that overlap period.
 
-  // All appointments are exactly 1 hour long.
-  // Customers need to be visited only one time by any employee and a
-  // single employee can not visit multiple customers at the same time.
+  //// All appointments are exactly 1 hour long.
+  //// Customers need to be visited only one time by any employee and a
+  //// single employee can not visit multiple customers at the same time.
 
-  // Please generate a list of `Appointment` structs that indicate all
-  // the appointments that have been scheduled between the employees and customers.
+  //// Please generate a list of `Appointment` structs that indicate all
+  //// the appointments that have been scheduled between the employees and customers.
+  //// --------------
 
+  // Large data to test on a full solution
+  // const {worker, customer} = large;
+
+  // Sample data to develop an initial solution
   const {worker, customer} = sample;
 
   return appointmentMocks;
