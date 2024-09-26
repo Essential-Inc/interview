7160861335ea9b1f0e29cddc9bf3d820b756f4ba
Tam Nguyen <tamsanh@gmail.com>
2024-09-26 12:53:50 -0500

    Reformat type reference

--- src/interview-scheduling/query.ts
+++ src/interview-scheduling/query.ts
@@ -1,39 +1,42 @@
 import { appointmentMocks, sample, large } from "@/interview-scheduling/data";
 import { Appointment } from "./types";
 
-// export type TimeRange = {
-//   // Inclusive of start
-//   // Exclusive of end
-//   start: Date;
-//   end: Date;
-// };
-//
-// export type WorkerAvailability = {
-//   workerId: string;
-//   workerName: string;
-//   availabilities: TimeRange[];
-// };
-//
-// export type CustomerPreference = {
-//   customerId: string;
-//   customerName: string;
-//   address: string;
-//   availabilities: TimeRange[];
-// };
-//
-// export type Database = {
-//   workers: WorkerAvailability[];
-//   customers: CustomerPreference[];
-// };
-//
-// export type Appointment = {
-//   workerId: string;
-//   customerId: string;
-//   customerName?: string;
-//   address: string;
-//   start: Date;
-//   end: Date;
-// };
+//// --------------
+//// NOTE: Type Reference for your Convenience
+//// export type TimeRange = {
+////   // Inclusive of start
+////   // Exclusive of end
+////   start: Date;
+////   end: Date;
+//// };
+////
+//// export type WorkerAvailability = {
+////   workerId: string;
+////   workerName: string;
+////   availabilities: TimeRange[];
+//// };
+////
+//// export type CustomerPreference = {
+////   customerId: string;
+////   customerName: string;
+////   address: string;
+////   availabilities: TimeRange[];
+//// };
+////
+//// export type Database = {
+////   workers: WorkerAvailability[];
+////   customers: CustomerPreference[];
+//// };
+////
+//// export type Appointment = {
+////   workerId: string;
+////   customerId: string;
+////   customerName?: string;
+////   address: string;
+////   start: Date;
+////   end: Date;
+//// };
+//// --------------
 
 export const getSchedules = (): Appointment[] => {
   //// --------------
