c5ec30883362986694ecb09f313b127ecbe16eb6
Tam Nguyen <tamsanh@gmail.com>
2024-09-26 12:30:17 -0500

    Duplicate readme

--- src/interview-scheduling/query.ts
+++ src/interview-scheduling/query.ts
@@ -37,6 +37,21 @@ import { Appointment } from "./types";
 
 export const getSchedules = (): Appointment[] => {
   // TODO: Implement this function
+
+  // The first task to complete is the scheduling of employees and customers.
+  // The data structure describing availability is a list of `TimeRange` structs,
+  // which describes the start and end of a range of time that an individual is available.
+
+  // If there is at least a 1 hour overlap between any time ranges for an employee and a customer,
+  // then an appointment can be scheduled for that overlap period.
+
+  // All appointments are exactly 1 hour long.
+  // Customers need to be visited only one time by any employee and a
+  // single employee can not visit multiple customers at the same time.
+
+  // Please generate a list of `Appointment` structs that indicate all
+  // the appointments that have been scheduled between the employees and customers.
+
   const {worker, customer} = sample;
 
   return appointmentMocks;
