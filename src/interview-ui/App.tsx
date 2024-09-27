f03a8c082a14c718002fc4a256e2a13c3ddc51fb
Tam Nguyen <tamsanh@gmail.com>
2024-09-27 16:08:01 -0400

    Fix bad queryKey

--- src/interview-ui/App.tsx
+++ src/interview-ui/App.tsx
@@ -11,7 +11,7 @@ import { getSchedules } from "../interview-scheduling";
 
 const App = () => {
   const { data } = useQuery({
-    queryKey: ["schedules", Math.random()],
+    queryKey: ["schedules"],
     queryFn: getSchedules,
   });
 
