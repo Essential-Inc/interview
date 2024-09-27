28c338e3a39e22ba84e521a24a516602eb965979
Tam Nguyen <tamsanh@gmail.com>
2024-09-27 16:08:49 -0400

    Refetch queries on mount

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { App } from "./interview-ui";
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: true,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
